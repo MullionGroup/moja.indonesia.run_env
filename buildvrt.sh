292bf3f55c0930553336e89bcc7c295178db21a9
mal <malcolm.francis@bowrush.com>
2019-10-09 16:21:08 +1100

    updated config for moja global

--- buildvrt.sh
+++ buildvrt.sh
@@ -1,3 +1,17 @@
-find -name '*_12.grd' > forestStemCM_12.txt
-gdalbuildvrt -input_file_list forestStemCM_12.txt forestStemCM_12.vrt
-rm forestStemCM_12.txt
+gdalbuildvrt forestStemCM_033041_12.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_12.tif
+gdalbuildvrt forestStemCM_033041_24.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_24.tif
+gdalbuildvrt forestStemCM_033041_36.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_36.tif
+gdalbuildvrt forestStemCM_033041_48.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_48.tif
+gdalbuildvrt forestStemCM_033041_60.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_60.tif
+gdalbuildvrt forestStemCM_033041_72.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_72.tif
+gdalbuildvrt forestStemCM_033041_84.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_84.tif
+gdalbuildvrt forestStemCM_033041_96.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_96.tif
+gdalbuildvrt forestStemCM_033041_108.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_108.tif
+gdalbuildvrt forestStemCM_033041_120.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_120.tif
+gdalbuildvrt forestStemCM_033041_132.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_132.tif
+gdalbuildvrt forestStemCM_033041_144.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_144.tif
+gdalbuildvrt forestStemCM_033041_156.vrt data/output/results/spatial_outputs/grids/forestStemCM/033041/*_156.tif
+
+
+gdalbuildvrt errors_033041.vrt data/output/results/spatial_outputs/grids/errors/033041/*.tif
+gdalbuildvrt simulate_land_unit_033041.vrt data/output/results/spatial_outputs/grids/simulate_land_unit/033041/*.tif
